fdf4397f5d36b6080afde40666528685ec0b26c4
PussyAss <Deadspace20111@yandex.ru>
2018-02-09 09:25:37 +0700

    lol

---
+++ pr1/pr1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+            // Создать объект Car по имени Chuck со скоростью 10 миль в час.
+            car chuck = new car();
+            chuck.PrintState();
+
+            // Создать объект Car по имени Mary со скоростью 0 миль в час.
+            car mary = new car("Mary");
+            mary.PrintState();
+
+            // Создать объект Car по имени Daisy со скоростью 75 миль в час.
+            car daisy = new car("Daisy", 75);
+            daisy.PrintState();
+
+            // Разместить в памяти и сконфигурировать объект Car.
+            car myCar = new car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+            // Увеличить скорость автомобиля в несколько раз и вывести новое состояние.
+            for (int i = 0; i <=10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pr1/pr1/car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    class car
+    {
+        // 'Состояние' объекта Car.
+        public string petName;
+        public int currSpeed;
+
+        //Специальный стандартный конструктор.
+        public car ()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+
+        // Здесь currSpeed получает стандартное значение для типа int {0}
+        public car(string pn)
+        {
+            petName = pn;
+        }
+
+        //Позволяет вызывающему коду установить полное состояние Car.
+        public car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+
+        // Функциональность Car.
+        {
+        public void PrintState ()
+        {
+           Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed)
+        }
+            public void SpeedUp (int delta)
+        {
+            currSpeed += delta;
+        }
+
+        }
+    }
+}
